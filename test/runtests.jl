5f28fd923719b70e81c37c1e8b873e7b30a8e9ed
Ronald @ home <r.a.remmerswaal@rug.nl>
2020-05-23 13:35:07 +0200

    Test gradient accuracy at refinement interfaces

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,30 +2,49 @@ using FiniteVolumeMethod
 using Test
 
 @testset "FiniteVolumeMethod.jl" begin
-    uniform = Grid([0.0, 0.0])
-    max_steps = 5
-    lInf_linear = zeros(max_steps)
-    lInf_nonlinear = zeros(max_steps)
+    dim = 2
+    uniform = Grid(zeros(dim))
+    max_steps = 3
+
+    # A uniform grid
     for step=1:max_steps
         FiniteVolumeMethod.refine!(uniform, [uniform.tree], recurse=true)
-
-        p = CellVar(x -> pi + 7x[1] - 4x[2], uniform)
-        gExact = FaceVar(x -> [7, -4], uniform)
-        g = gradient(p, uniform)
-        lInf_linear[step] = maximum(abs.(g.data - gExact.data))
-
-        p = CellVar(x -> pi + 7sin(x[1]) - 4x[1]x[2]^2, uniform)
-        gExact = FaceVar(x -> [7cos(x[1]) - 4x[2]^2, -8x[1]x[2]], uniform)
-        g = gradient(p, uniform)
-        lInf_nonlinear[step] = maximum(abs.(g.data - gExact.data))
     end
 
-    # Gradient of a linear function should be exact
-    @test all(lInf_linear .== 0.0)
+    # Locally refined grid
+    refined = Grid(zeros(dim))
+    child = refined.tree
+    for step=1:max_steps
+        FiniteVolumeMethod.refine!(uniform, [child])
+        child = child.children[1+mod(step-1, 4)]
+    end
 
-    # Gradient of a nonlinear function should be second order accurate
-    order = log2.(lInf_nonlinear[1:max_steps-1] ./ lInf_nonlinear[2:max_steps])
-    @test all(abs.(order .- 2) .< 1E-2)
+    grids = [uniform, refined]
+    orders = [2, 1]
 
+    lInf_linear = zeros(max_steps)
+    lInf_nonlinear = zeros(max_steps)
+    for (i, grid) ∈ enumerate(grids)
+        for step=1:max_steps
+            FiniteVolumeMethod.refine!(grid, [grid.tree], recurse=true)
 
+            p = CellVar(x -> pi + 7x[1] - 4x[2], grid)
+            gExact = FaceVar(x -> [7, -4], grid)
+            g = gradient(p, grid)
+            lInf_linear[step] = maximum(abs.(g.data - gExact.data))
+
+            p = CellVar(x -> pi + 7sin(x[1]) - 4x[1]x[2]^2, grid)
+            gExact = FaceVar(x -> [7cos(x[1]) - 4x[2]^2, -8x[1]x[2]], grid)
+            g = gradient(p, grid)
+            lInf_nonlinear[step] = maximum(abs.(g.data - gExact.data))
+        end
+
+        # Gradient of a linear function should be exact
+        @test all(lInf_linear .== 0.0)
+
+        # Gradient of a nonlinear function should be second order accurate (uniform)
+        # and first order accurate at refinement interfaces
+        order = log2.(lInf_nonlinear[1:max_steps-1] ./ lInf_nonlinear[2:max_steps])
+        @test all(abs.(order .- orders[i]) .< 1E-1)
+    end
 end
